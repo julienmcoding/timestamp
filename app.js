2940df36a8e4c1a7cf6f497f1bc68c39d686fbaf
github_julienmcoding <manois.julien@gmail.com>
2021-05-28 19:02:47 +0200

    port heroku

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/api/:input', (req, res) => {
     res.json(response);
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is listenning on port ${PORT}`);
