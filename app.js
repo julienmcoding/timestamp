ef42bf7fac0507c1b9205218d6b546e38c335d3e
github_julienmcoding <manois.julien@gmail.com>
2021-05-31 14:08:38 +0200

    wasnt passing the test

--- app.js
+++ app.js
@@ -14,8 +14,21 @@ app.get('/api', (req, res) => {
     res.render('index');
 });
 
+const response = {};
+
 app.get('/api/:input', (req, res) => {
-    res.render('input', {input: req.params.input});
+    const input = req.params.input;
+    if(input.includes('-')) {
+        response['unix'] = new Date(input).getTime();
+        response['utc'] = new Date(input).toUTCString();
+    } else {
+        response['unix'] = new Date(parseInt(input)).getTime();
+        response['unix'] = new Date(parseInt(input)).toUTCString();
+    };
+    res.json(response);
+    if (!response['unix'] || !response['unix']) {
+        res.json({ error : "Invalid Date" });
+    }
 });
 
 const PORT = process.env.PORT || 3000;
