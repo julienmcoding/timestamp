1fcaea5a3f614143ee28a5c0fe80b7fd5bb49547
github_julienmcoding <manois.julien@gmail.com>
2021-05-31 09:21:00 +0200

    date conversion

--- app.js
+++ app.js
@@ -5,13 +5,29 @@ app.use(express.json());
 
 const response = {};
 
+app.get('/', (req, res) => {
+    res.send('Hello you!');
+});
 
 app.get('/api/:input', (req, res) => {
     let input = req.params.input;
     if(input.includes('-')) {
         response['unix'] = new Date(input).getTime();
         response['utc'] = new Date(input).toUTCString();
+    } else {
+        response['unix'] = new Date(parseInt(input)).getTime();
+        response['utc'] = new Date(parseInt(input)).toUTCString();
+
     }
+    if (!response['unix'] || response['utc']) {
+        res.json({ error : "Invalid Date" });
+    };
+    res.json(response);
+});
+
+app.get('/api', (req, res) => {
+    response['unix'] = new Date().getTime();
+    response['utc'] = new Date().toUTCString();
     res.json(response);
 });
 
